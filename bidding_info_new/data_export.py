6d91e56535fe4d775266172baaa5e56857544e99
walson <298100732@qq.com>
2021-01-11 15:16:56 +0800

    20210111

--- bidding_info_new/data_export.py
+++ bidding_info_new/data_export.py
@@ -9,11 +9,53 @@ info_detail = pd.read_sql_table('info_detail', engine)
 combine_data = pd.DataFrame()
 
 i = 0
+for index , row in info_detail.iterrows():
+    # print(index)
+    sub_title = row[1].split('|')
+    item = row[2].split('|')
 
-for title in info_detail['bidding_table_titles_list']:
-    sub_title = title.split('|')
-    print(sub_title)
-    i = i + 1
+
+    i = 0
+    pop_list = []
+    while i < len(sub_title):
+        # print(sub_title[i])
+        if sub_title[i] == '':
+            print("x")
+            pop_list.append(i)
+        i += 1
+
+    if len(pop_list) > 0 or len(sub_title) != len(item):
+        continue;
+
+
+    new_dict = dict(zip(sub_title,item))
+    new_pd = pd.DataFrame(data=new_dict, index=[0], columns=sub_title)
+
+
+    # new_pd = new_pd.drop('',axis=1)
+    print(index)
+    # print(new_pd)
+    if index == 0 :
+        combine_data = new_pd
+    else:
+        # combine_data = combine_data.reset_index()
+        # new_pd = new_pd.reset_index()
+        try:
+            combine_data = pd.concat([combine_data, new_pd] , axis=0, ignore_index=True)
+        except:
+            print("出错了")
+    # print(combine_data)
+    
+
+combine_data.to_excel('final_format.xlsx', encoding='utf8')
+
+
+
+
+# for title in info_detail['bidding_table_titles_list']:
+#     sub_title = title.split('|')
+#     print(sub_title)
+#     i = i + 1
     # sub_item = info_detail['bidding_table_items_list'][i].split(',')
 
     # print(len(sub_item))
@@ -25,6 +67,7 @@ for title in info_detail['bidding_table_titles_list']:
 
     # print(title.split(','))
 # for item in info_detail['bidding_table_items_list']:
-#     print(item.split(','))
-        # single_data = pd.DataFrame(data=item,columns=title)
-        # print(single_data)
+#     single_data = pd.DataFrame(np.array(item.split('|')))
+#     print(single_data)
+    # print(item.split('|'))
+    # print(single_data)
